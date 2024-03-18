1bfa7e4a3316bccff8ec5edd9d485c7b210e1ef8
Gideon kirui <kipyegongideon99@gmail.com>
2024-03-18 17:51:33 +0300

    string lab review phase 1

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const currentUser = 'Gideon Kirui';
+
 const welcomeMessage = 'Welcome to Flatbook, '+ currentUser+ "!";
+
 const  excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
-const shortGreeting = "Welcome, "+ currentUser.charAt(0) + "!";
+
+const shortGreeting = "Welcome, "+ currentUser.slice(0 ,8) + "!";
